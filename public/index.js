4bb0e1832297a1e9507f97df05f47009a6c0f99a
unknown <lluvporwall@gmail.com>
2023-08-14 16:33:29 +0530

    ExactSpace company  assignment completed

--- public/index.js
+++ public/index.js
@@ -6,7 +6,6 @@ Form.addEventListener("submit", async (event) => {
   event.preventDefault();
   const inputData = document.getElementById("inputData").value;
   try {
- 
     const parseData = JSON.parse(inputData);
     const res = await fetch("/json-data", {
       method: "POST",
@@ -27,7 +26,7 @@ Form.addEventListener("submit", async (event) => {
       timer: 2500,
     });
 
-    // Call the 'display' function to display the response
+    // Calling the 'display' function to display the response
     display(temp);
   } catch (error) {
     Swal.fire({
@@ -38,13 +37,15 @@ Form.addEventListener("submit", async (event) => {
   }
 });
 
-// displaying the input data in the 'formContainer'
+// displaying the input data in the 'formContainer' div
 function display(data) {
   const formContainer = document.getElementById("formContainer");
   formContainer.style.display = "block";
   formContainer.innerHTML = "";
-  const div = document.createElement("div");
+  const cont = document.createElement("div");
   for (let key in data) {
+    const div = document.createElement("div");
+    div.setAttribute("class", "output-div");
     const label = document.createElement("label");
     label.innerText = key;
     label.setAttribute("class", "output-label");
@@ -54,7 +55,8 @@ function display(data) {
     input.value = data[key];
     input.setAttribute("class", "output-input");
     const lineBreak = document.createElement("br");
-    div.append(label, input,lineBreak);
+    div.append(label, input, lineBreak);
+    cont.append(div);
   }
-  formContainer.append(div);
+  formContainer.append(cont);
 }
